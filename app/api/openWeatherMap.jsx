4ffa88b1036b2b08c9a62cb73a84aab2b3c6b93f
naveenreddysg <naveenreddy.sg@gmail.com>
2018-05-14 13:33:11 +0530

    add custom style

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -20,7 +20,7 @@ module.exports = {
                 return res.data.main.temp;
             }
         },function(err){
-            throw new Error(err.response.data.message)
+            throw new Error(err.response.data.message);
             // throw new Error('city not found');
         })
     }
