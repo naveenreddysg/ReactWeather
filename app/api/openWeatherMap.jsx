65682c0ffb76e09c0a5f79d6e73b5f4ab30b03dc
naveenreddysg <naveenreddy.sg@gmail.com>
2018-05-14 15:17:41 +0530

    modified changes

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=312c2e8c1b831c2f34ef1ec389fafb9c&units=imperial';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=c4e735ea8bd7e7b6dc8368c752517b2d&units=imperial';
 
 module.exports = {
   getTemp: function (location) {
@@ -14,8 +14,7 @@ module.exports = {
         return res.data.main.temp;
       }
     }, function (res) {
-        throw new Error(err.response.data.message);
-        // throw new Error('city not found');
+      throw new Error(res.data.message);
     });
   }
 }
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,31 +1,5 @@
 var React = require('react');
 
-// var WeatherMessage = React.createClass({
-//     render: function(){
-//         var {temp, location} = this.props;
-
-//         return(
-//             <h3>It's is {temp} in {location}</h3>
-//         );
-//     }
-// });
-
-//or
-
-//Arrow Function
-
-// var WeatherMessage = (props) => {
-//     var {temp, location} = props;
-
-//         return(
-//             <h3>It's is {((temp-32)*(5/9)).toFixed(2)} &#8451; in {location}</h3>
-//         );
-// }
-
-//or
-
-//Arrow Function
-
 var WeatherMessage = ({temp, location}) => {
   return (
     <h3 className="text-center">It's it {temp} in {location}.</h3>
